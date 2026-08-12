0797e41e19642e5e7f7ea698c2b0360fce9f37e7
Ahmed Hamed <ahmed@example.com>
2026-08-12 10:52:02 +0300

    Add DevSecOps CI/CD and Kubernetes monitoring

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,19 +2,27 @@ from fastapi import FastAPI
 
 from app.database import Base, engine
 from app import models
-
 from app.routes import auth, tasks
 
+from prometheus_fastapi_instrumentator import Instrumentator
 
+
+# Create database tables
 Base.metadata.create_all(bind=engine)
 
 
+# Create FastAPI application
 app = FastAPI(
     title="Secure Task Management API",
     version="1.0.0"
 )
 
 
+# Prometheus metrics
+Instrumentator().instrument(app).expose(app)
+
+
+# Routers
 app.include_router(auth.router)
 app.include_router(tasks.router)
 
